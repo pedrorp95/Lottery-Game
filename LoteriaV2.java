49e7ad8f631819e09484c99a126c7dbdda9aa91f
Pedro Rodríguez Pérez <pedro.rp95@gmail.com>
2022-07-21 00:03:56 +0200

    Compra de boletoEuromillon, premios asignados solo queda consultar bole tos comprados, sumar los premios y que no se acumulen

--- LoteriaV2.java
+++ LoteriaV2.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 public class LoteriaV2 {
     //Variables globales
     static double cartera=0.0;
+    static double compra=0.0;
     static int cantidadBoletos = 0;
+    static int cantidadBoletosEuro =0;
     static int premio;
     static int BoletosLoteria[];
+    static int BoletosEuro[][];
+    static int BoletosEuroPremio [] = new int [7];
     static ArrayList<Integer> ListaBoletos = new ArrayList<Integer>();
     
     
@@ -23,7 +27,7 @@ public class LoteriaV2 {
             System.out.println("3. Compra boletos Loteria");
             System.out.println("4. Consulta sus boletos Loteria");
             System.out.println("5. Premios de la loteria");
-            System.out.println("6. Compra y consulta de boletos Euromillon");
+            System.out.println("6. Compra boletos Euromillon");
             System.out.println("7. Premios Euromillon");
             System.out.println("9. Salir");
 
@@ -45,7 +49,11 @@ public class LoteriaV2 {
                 case 5:
                     PremioLoteria();
                     break;
-                default:
+                case 6:
+                    CompraBoletosEuro();
+                    break;
+                case 7:
+                    PremiosEuro();
                     break;
             }
         } while (opcionMenu !=9);
@@ -64,7 +72,6 @@ public class LoteriaV2 {
     public static void CompraBoletosLoteria(){
         int manualmente = 1000;
         int nmanual = 0;
-        double compra=0;
         System.out.println("¿Cuántos boletos quieres comprar?");
         cantidadBoletos = TecladoInt();
 
@@ -99,7 +106,7 @@ public class LoteriaV2 {
                 for (int i : BoletosLoteria) {
                     ListaBoletos.add(i);
                 }
-                cartera -= cantidadBoletos*3;
+                cartera -= compra;
             }
         }
 
@@ -123,7 +130,7 @@ public class LoteriaV2 {
             for (int i : BoletosLoteria) {
                 ListaBoletos.add(i);
             }
-            cartera -= cantidadBoletos*3;
+            cartera -= compra;
         }
     }
 
@@ -182,6 +189,199 @@ public class LoteriaV2 {
             }
         }
         System.out.println("Ha ganado un total de: "+dinero+"$");
+    }
+
+    
+    public static void CompraBoletosEuro(){ //AÑLDKFJALÑDKFJAÑLDJKF
+        int aleatorio = 0;
+        int manualmente = 1000;
+        int nmanual = 0;
+        boolean insuficiente = false;
+
+        System.out.println("¿Cuántos boletos de euromillón quieres comprar?");
+        cantidadBoletosEuro = TecladoInt();
+
+        BoletosEuro = new int [cantidadBoletosEuro][7];
+        compra = cantidadBoletosEuro * 2.5;
+
+        if (compra > cartera) {
+            insuficiente = true;
+            System.out.println("No tiene suficiente saldo");
+        }
+
+        if (insuficiente == false) {
+            do {
+                System.out.println("¿Aleatorio (0) o Manualmente (1)?");
+                manualmente = TecladoInt();
+            } while (manualmente != 0 && manualmente != 1);
+        }
+
+        
+
+        if (manualmente == 0) { //Forma aleatoria
+            if (insuficiente == false) {
+                for (int i = 0; i <  cantidadBoletosEuro; i++) { //Para los 5 primeros numeros
+                    for (int j = 0; j<5; j++) {
+                        aleatorio = Aleatorio(50, 1);
+                        BoletosEuro[i][j] = aleatorio;
+                    }
+                    for (int j = 0; j<2; j++) { //Para las dos estrellas del numero
+                        aleatorio = Aleatorio(10, 1);
+                        BoletosEuro[i][j+5] = aleatorio;
+                    }
+                }
+
+                for (int i = 0; i < cantidadBoletosEuro; i++) {
+                    for (int j = 0; j < 7; j++) {
+                        System.out.print(BoletosEuro[i][j]+" ");
+                    }
+                }
+                System.out.println("\n");
+                cartera -= compra;
+            }
+        }
+
+        if (manualmente == 1) { //Forma manual
+            int numeros =0;
+            int estrellas=0;
+
+            if (insuficiente == false) {
+                System.out.println("Por favor introduzca los 5 primeros digitos entre 1 y 49");
+                for (int i = 0; i < cantidadBoletosEuro; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        if (numeros < 5) {
+                            do {
+                                nmanual = TecladoInt();
+                                BoletosEuro[i][j] = nmanual;
+                                if (nmanual <1 || nmanual >49) {
+                                    System.out.println("Numeros entre 1 y 49");
+                                }
+                            } while (nmanual <1 || nmanual >49);
+                            numeros++;
+                        }
+                    }
+
+                    System.out.println("Por favor introduce los números de las estrellas, digitos entre 1 y 9");
+                    for (int j = 0; j < 2; j++) {
+                        if (estrellas <2) {
+                            do {
+                                nmanual = TecladoInt();
+                                BoletosEuro[i][j+5] = nmanual;
+                                if (nmanual <1 || nmanual >9) {
+                                    System.out.println("Numeros entre 1 y 9");
+                                }
+                            } while (nmanual <1 || nmanual >9);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < cantidadBoletosEuro; i++) {
+                    for (int j = 0; j < 7; j++) {
+                        System.out.print(BoletosEuro[i][j]+" ");
+                    }
+                }
+                System.out.print("¡Buena Suerte!"+"\n");
+                cartera -= compra;
+            }
+        }
+    }
+
+    public static void PremiosEuro(){
+        int numeroserie=0;
+        int estrellas=0;
+        int naleatorio;
+
+        for (int i = 0; i < 5; i++) { //5 primeros numeros del boleto euro
+            naleatorio = Aleatorio(50, 1);
+            BoletosEuroPremio[i] = naleatorio;
+        }
+
+        for (int i = 0; i < 2; i++) { //2 ultimas estrellas
+            naleatorio = Aleatorio(10, 1);
+            BoletosEuroPremio[i+5] = naleatorio;
+        }
+        System.out.println("El numero premiado es: ");
+        for (int i = 0; i < BoletosEuroPremio.length; i++) {
+            System.out.print(BoletosEuroPremio[i]+" ");
+        }
+        System.out.println("\n");
+
+        
+        
+        for (int i = 0; i < cantidadBoletosEuro; i++) {
+            
+            for(int j=0; j<5; j++){
+                if(BoletosEuro[i][j] == BoletosEuroPremio[0]) numeroserie++;
+                if(BoletosEuro[i][j] == BoletosEuroPremio[1]) numeroserie++;
+                if(BoletosEuro[i][j] == BoletosEuroPremio[2]) numeroserie++;
+                if(BoletosEuro[i][j] == BoletosEuroPremio[3]) numeroserie++;
+                if(BoletosEuro[i][j] == BoletosEuroPremio[4]) numeroserie++;
+            }
+
+            for(int j=5; j<7; j++){
+                if(BoletosEuro[i][j] == BoletosEuroPremio[5]) estrellas++;
+                if(BoletosEuro[i][j] == BoletosEuroPremio[6]) estrellas++;
+                }
+
+            if (numeroserie <2) {
+                System.out.println("No te ha tocado nada");
+            }
+            else if(numeroserie==2 && estrellas==0){
+                naleatorio=Aleatorio(7, 1);
+                System.out.println("El premio ha sido: "+naleatorio+"💵");
+                continue;
+            }
+            else if(numeroserie==2 && estrellas==1){
+                naleatorio=Aleatorio(8, 4);
+                System.out.println("El premio ha sido: "+naleatorio+"💵");
+                continue;
+            }
+            else if(numeroserie==3 && estrellas==1){
+                naleatorio=Aleatorio(16, 8);
+                System.out.println("El premio ha sido: "+naleatorio+"💵");
+                continue;
+            }
+            else if(numeroserie==2 && estrellas==2){
+                naleatorio=Aleatorio(21, 10);
+                System.out.println("El premio ha sido: "+naleatorio+"💵");
+                continue;
+            }
+            else if(numeroserie==4 && estrellas==0){
+                naleatorio=Aleatorio(61, 20);
+                System.out.println("El premio ha sido: "+naleatorio+"💵");
+                continue;
+            }
+            else if(numeroserie==3 && estrellas==2){
+                naleatorio=Aleatorio(81, 30);
+                System.out.println("El premio ha sido: "+naleatorio+"💵");
+                continue;
+            }
+            else if(numeroserie==4 && estrellas==1){
+                naleatorio=Aleatorio(601, 90);
+                System.out.println("El premio ha sido: "+naleatorio+"💵");
+                continue;
+            }
+            else if(numeroserie==4 && estrellas==2){
+                naleatorio=Aleatorio(6001, 750);
+                System.out.println("El premio ha sido: "+naleatorio+"💵");
+                continue;
+            }
+            else if(numeroserie==5 && estrellas==0){
+                naleatorio=Aleatorio(120001, 80000);
+                System.out.println("El premio ha sido: "+naleatorio+"💵");
+                continue;
+            }
+            else if(numeroserie==5 && estrellas==1){
+                naleatorio=Aleatorio(2000001, 600000);
+                System.out.println("El premio ha sido: "+naleatorio+"💵");
+                continue;
+            }
+            else if(numeroserie==5 && estrellas==2){
+                naleatorio=Aleatorio(900000001, 75000000);
+                System.out.println("El premio ha sido: "+naleatorio+"💵");
+                continue;
+            }
+        }
     }
 
     public static int TecladoInt(){
